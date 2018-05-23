0932bf311637574c7de6d4463e08b30b9b44c953
Farooq Asmaa <farooq.asmaa@gmail.com>
2018-05-23 20:10:07 +0200

    Add functions + gitignore

--- index.js
+++ index.js
@@ -34,6 +34,9 @@ spotifyApi.clientCredentialsGrant().then(
 
     // Save the access token so that it's used in future calls
     spotifyApi.setAccessToken(data.body['access_token']);
+
+    sendSpotifyData();
+    sendDeezerData();
   },
   function(err) {
     console.log(
@@ -46,127 +49,132 @@ spotifyApi.clientCredentialsGrant().then(
 // spotifyApi.setAccessToken('BQBSkrolgsn4JhO5W1GYfWfs29XqMITNIEz0kID8wEnL5HWQ-xqUIExLIT1PT9o_F9Vd1GS3AJB_x4DxjRI');
 
 // API ENDPOINTS :
-// setTimeout(function() {
-//   // send Spotify TOP FR into DB
-//   spotifyApi.getFrenchChart(spotifyUserChart, spotifyChartId)
-//     .then(function(data) {
-//       let trackTitle = "";
-//       let trackArtist = "";
-//       let trackAlbum = "";
+function sendSpotifyData() {
+  // send Spotify TOP FR into DB
+  spotifyApi.getFrenchChart(spotifyUserChart, spotifyChartId)
+    .then(function(data) {
+      let trackTitle = "";
+      let trackArtist = "";
+      let trackAlbum = "";
 
-//       for(var exKey in data.body.tracks) {
-//         if(exKey == "items" && typeof data.body.tracks[exKey] == "object") {
+      for(var exKey in data.body.tracks) {
+        if(exKey == "items" && typeof data.body.tracks[exKey] == "object") {
 
-//           for(let trackKey in data.body.tracks[exKey]) {
-//             for(let underTrackKey in data.body.tracks[exKey][trackKey]) {
-//               if(underTrackKey == "track") {
+          for(let trackKey in data.body.tracks[exKey]) {
+            for(let underTrackKey in data.body.tracks[exKey][trackKey]) {
+              if(underTrackKey == "track") {
 
-//                 for(let objKey in data.body.tracks[exKey][trackKey]) {
-//                   if(objKey != "track") {
-//                     continue;
-//                   }
+                for(let objKey in data.body.tracks[exKey][trackKey]) {
+                  if(objKey != "track") {
+                    continue;
+                  }
 
-//                   trackTitle = data.body.tracks[exKey][trackKey][objKey].name;
+                  trackTitle = data.body.tracks[exKey][trackKey][objKey].name;
 
-//                   for(let key in data.body.tracks[exKey][trackKey][objKey]) {
-//                     if(key == "album") {
-//                       for(let k in data.body.tracks[exKey][trackKey][objKey][key]) {
-//                         trackAlbum = data.body.tracks[exKey][trackKey][objKey][key].name;
-//                       }
-//                     }
+                  for(let key in data.body.tracks[exKey][trackKey][objKey]) {
+                    if(key == "album") {
+                      for(let k in data.body.tracks[exKey][trackKey][objKey][key]) {
+                        trackAlbum = data.body.tracks[exKey][trackKey][objKey][key].name;
+                      }
+                    }
 
-//                     if(key == "artists") {
-//                       for(let k in data.body.tracks[exKey][trackKey][objKey][key]) {
-//                         data.body.tracks[exKey][trackKey][objKey][key].forEach(function(item, index) {
-//                           trackArtist = item.name;
-//                           // console.log("trackArtist :", trackArtist);
-//                         });
-//                       }
-//                     }
-//                   }
+                    if(key == "artists") {
+                      for(let k in data.body.tracks[exKey][trackKey][objKey][key]) {
+                        data.body.tracks[exKey][trackKey][objKey][key].forEach(function(item, index) {
+                          trackArtist = item.name;
+                          // console.log("trackArtist :", trackArtist);
+                        });
+                      }
+                    }
+                  }
 
-//                   // let track = new Track({title: trackTitle, artist: trackArtist, album: trackAlbum});
-//                   /* save into db */
-//                   // Track.findOrCreate({
-//                   //   where: {
-//                   //     title: trackTitle,
-//                   //     artist: trackArtist,
-//                   //     album: trackAlbum
-//                   //   }
-//                   // }).then(function(result) {
-//                   //     var track = result[0], // the instance of the author
-//                   //       created = result[1]; // boolean stating if it was created or not
+                  // let track = new Track({title: trackTitle, artist: trackArtist, album: trackAlbum});
+                  /* save into db */
+                  Track.findOrCreate({
+                    where: {
+                      title: trackTitle,
+                      artist: trackArtist,
+                      album: trackAlbum
+                    }
+                  }).then(function(result) {
+                      var track = result[0], // the instance of the track
+                        created = result[1]; // boolean stating if it was created or not
 
-//                   //     if (created) {
-//                   //       console.log('Track already exists');
-//                   //     }
+                      if (created) {
+                        console.log('Track already exists');
+                      }
 
-//                   //     console.log('Created track...');
-//                   //   });
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-
-//     }, function(err) {
-//       console.error(err);
-//     });
-// }, 1000);
-
-
-// send Deezer TOP FR into DB
-dz.getFrenchChart(deezerChartId).then(function(album) {
-
-  let trackTitle = "";
-  let trackArtist = "";
-  let trackAlbum = "";
-
-  for(var exKey in album.tracks) {
-    if(exKey == "checksum") {
-      continue;
-    }
-
-    for(let idx in album.tracks[exKey]) {
-      album.tracks[exKey].forEach(function(subItem, subIdx) {
-        trackTitle = subItem.title;
-
-        for(let subItemKey in subItem) {
-          if(subItemKey == "artist") {
-            for(let artistKey in subItem[subItemKey]) {
-              trackArtist = subItem[subItemKey].name;
-            }
-          }
-
-          if(subItemKey == "album") {
-            for(let albumKey in subItem[subItemKey]) {
-              trackAlbum = subItem[subItemKey].title;
+                      console.log('Created track...');
+                    });
+                }
+              }
             }
           }
         }
+      }
 
-        /* save into db */
-        Track.findOrCreate({
-          where: {
-            title: trackTitle,
-            artist: trackArtist,
-            album: trackAlbum
-          }
-        }).then(function(result) {
-            var track = result[0], // the instance of the author
-              created = result[1]; // boolean stating if it was created or not
+    }, function(err) {
+      console.error(err);
+    });
+}
 
-            if (created) {
-              console.log('Track already exists');
+
+function sendDeezerData() {
+  // send Deezer TOP FR into DB
+  dz.getFrenchChart(deezerChartId).then(function(album) {
+
+    let trackTitle = "";
+    let trackArtist = "";
+    let trackAlbum = "";
+
+    for(var exKey in album.tracks) {
+      if(exKey == "checksum") {
+        continue;
+      }
+
+      for(let idx in album.tracks[exKey]) {
+        album.tracks[exKey].forEach(function(subItem, subIdx) {
+          trackTitle = subItem.title;
+
+          for(let subItemKey in subItem) {
+            if(subItemKey == "artist") {
+              for(let artistKey in subItem[subItemKey]) {
+                trackArtist = subItem[subItemKey].name;
+              }
             }
 
-            console.log('Created track...');
-          });
-      });
+            if(subItemKey == "album") {
+              for(let albumKey in subItem[subItemKey]) {
+                trackAlbum = subItem[subItemKey].title;
+              }
+            }
+          }
+
+          /* save into db */
+          Track.findOrCreate({
+            where: {
+              title: trackTitle,
+              artist: trackArtist,
+              album: trackAlbum
+            }
+          }).then(function(result) {
+              var track = result[0], // the instance of the track
+                created = result[1]; // boolean stating if it was created or not
+
+              if (created) {
+                console.log('Track already exists');
+              }
+
+              console.log('Created track...');
+            });
+        });
+      }
     }
-  }
-});
+  }, function(err) {
+    console.error(err);
+  });
+}
+
 
 // root
 app.get('/', (req, res) => {
